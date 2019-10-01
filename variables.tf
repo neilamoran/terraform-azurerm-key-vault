5271cee08f5a0506ac247614b7e831a14c5b4a0c
neilamoran <neilamoran@users.noreply.github.com>
2019-10-01 08:32:00 +0100

    Add empty lists for self-assigned access (certs/storage)
    This prevents an error when authenticated as a service principal.

--- variables.tf
+++ variables.tf
@@ -124,5 +124,7 @@ locals {
     tenant_id          = data.azurerm_client_config.main.tenant_id
     key_permissions    = ["create", "delete", "get"]
     secret_permissions = ["delete", "get", "set"]
+    certificate_permissions = []
+    storage_permissions = []
   }
 }
